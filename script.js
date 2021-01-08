5b78351b17d48713267e0d84ae27213cdb94baeb
SAYUK09 <sayurikamble123@gmail.com>
2021-01-08 18:41:54 +0530

    EventListerner added & Fetch URL working

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const btn = document.querySelector(".translateBtn")
 const outputDiv = document.querySelector(".outputDiv")
 // console.log(outputDiv)
 
+btn.addEventListener('click',()=>{
+    let inputText = txtInput.value;
+    console.log(inputText)
+    translatedURL (inputText)
+    
+})
+
+const serverURl = "https://api.funtranslations.com/translate/minion.json"
+
+function translatedURL (Text){
+    finalURL = serverURl + "?text=" + Text;
+    // return finalURL
+    
+    fetch(finalURL)
+    .then(response=>response.json())
+    .then(json=>{
+        console.log(json)
+    })
+   
+}
+
