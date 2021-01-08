8d26d55d326ce067d44c351fa53b4af8248db4d7
SAYUK09 <sayurikamble123@gmail.com>
2021-01-08 18:23:25 +0530

    Basic Structure and Access to DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+const txtInput = document.querySelector("#txtInput");
+// console.log(txtInput)
+const btn = document.querySelector(".translateBtn")
+// console.log(btn)
+const outputDiv = document.querySelector(".outputDiv")
+// console.log(outputDiv)
+
